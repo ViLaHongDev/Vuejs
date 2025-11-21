b6fe9bf6b1753de46b90fd8fd214c79a5a6c1fcb
ViDev <vixhoongla@gmail.com>
2025-11-21 15:58:21 +0700

    day1 - the first project created with Vuejs

---
+++ my-vue-app/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const count = 1;
+    const message = "Hello World!";
+</script>
+<template>
+    <p>{{ message }}</p>
+    <span>{{ count }}</span>
+    <div>
+    <h2>Xin chào! Đây là component đầu tiên của bạn 🎉</h2>
+    <p>Tôi được tạo trong thư mục components.</p>
+  </div>
+</template>
+<style>
+
+</style>
