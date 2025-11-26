2490a745d8bcb1c2f6e93d521e4e12910a708bd8
ViDev <vixhoongla@gmail.com>
2025-11-26 21:41:37 +0700

    day4 - emit

---
+++ day4-emit/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue';
+  import PostForm from './views/PostForm.vue';
+
+    const posts = ref([])
+
+    // Hàm nhận dữ liệu từ child
+    function addPost(newPost) {
+      posts.value.push(newPost)
+    }
+
+</script>
+
+<template>
+    <h1>Trang đăng bài</h1>
+    <PostForm @submit-post="addPost"></PostForm>
+    <h2>Danh sách bài viết</h2>
+    <ul>
+      <li v-for="(post,index) in posts" :key="index">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.content }}</p>
+        <p>{{ post.author }}</p>
+      </li>
+      
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ day4-emit/src/views/PostForm.vue
@@ -0,0 +1,51 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const title = ref('')
+    const content = ref('')
+    const author = ref('')
+
+    const emit = defineEmits(['submit-post']);
+
+    function handleSubmit() {
+
+        if(title.value && content.value && author.value){
+            const newPost = {
+                title : title.value,
+                content : content.value,
+                author : author.value
+            }
+            // Gửi dữ liệu lên parent thông qua emit
+            emit('submit-post',newPost);
+
+             // reset form
+            title.value = ''
+            content.value = ''
+            author.value = ''
+
+        }
+
+        
+    }
+
+</script>
+<template>
+    <form>
+        <div>
+            <label for="title">Tiêu đề</label>
+            <input type="text" v-model="title" placeholder="Nhập tiêu đề....">
+        </div>
+        <div>
+            <label for="content">Nội dung</label>
+            <input type="text" v-model="content" placeholder="Nhập nội dung....">
+        </div>
+        <div>
+            <label for="author">Tác giả</label>
+            <input type="text"v-model="author" placeholder="Nhập tác giả">
+        </div>
+        <button type="button" @click="handleSubmit">ĐĂNG BÀI</button>
+    </form>
+</template>
+<style scoped>
+
+</style>
