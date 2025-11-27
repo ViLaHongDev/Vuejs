3022c66a88f2b3eae191d2b5d60e25b9f575c349
ViDev <vixhoongla@gmail.com>
2025-11-27 09:35:54 +0700

    day5 - component basic

---
+++ day5-componentBasic/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import MyComponent from './components/MyComponent.vue';
+</script>
+
+<template>
+    <MyComponent title="Nghĩ khác đi, sẽ......." content="Bạn muốn học theo dạng lý thuyết trước hay nhảy vào thực hành tạo 1 component thật?"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ day5-componentBasic/src/components/MyComponent.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+
+    // // Declarin variable
+    // const message = 'Hello World!';
+
+    // // number
+    // const variable = 10;
+
+    // // 
+    // const aBoolean = true;
+
+    // // Object
+    // const person = {firstName:"John", lastName:"Doe"};
+
+    // // Array object
+    // const cars = ["Saab", "Volvo", "BMW"];
+
+
+    // const count = ref(0);
+
+    // function increment(){
+    //       count.value++
+    // }
+
+    const post = defineProps({
+        title : String,
+        content : String
+    })
+</script>
+
+<template>
+    <!-- <h1>{{ message }}</h1>
+    <h2>{{ variable }}</h2>
+    <h3>{{ aBoolean }}</h3>
+    <h4>{{ person.firstName}}</h4>
+    <h5>{{ cars[0] }}</h5> -->
+
+    <!-- <button @click="increment">Click {{ count }}</button> -->
+
+     <div class="post">
+        <h3>{{ title }}</h3>
+        <p>{{ content }}</p>
+    </div>
+</template>
+
+<style scoped>
+  
+</style>
